413590b810d0a5caf23d635bbc2d9fe6c6434a0a
anupmac6 <anupmac6@gmail.com>
2022-09-21 08:23:28 -0400

    initial commit

---
+++ ocr/README.md
@@ -0,0 +1,8 @@
+
+### Installing and Running
+
+
+```
+$ docker build -t anupmac6/ocr .
+$ docker run -d -p 5001:5001 anupmac6/ocr
+```
---
+++ ocr/app.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template, request,jsonify
+from werkzeug import secure_filename
+import os
+import sys
+from PIL import Image
+import pytesseract
+import argparse
+import cv2
+import re
+
+__author__ = 'Rick Torzynski <ricktorzynski@gmail.com>'
+__source__ = ''
+
+app = Flask(__name__)
+UPLOAD_FOLDER = './static/uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
+
+@app.route("/")
+def index():
+  return render_template("index.html")
+
+@app.route("/languages")
+def getLanguages():
+  languages = pytesseract.get_languages()
+  return jsonify(languages)
+
+@app.route("/about")
+def about():
+  return jsonify({'hi':'hello'})
+
+@app.route("/process")
+def process():
+
+  data = pytesseract.image_to_data(Image.open('tests/test.jpg'))
+  dataList = data.split('\n')
+  return jsonify(dataList)
+
+@app.route("/hindi")
+def hindi():
+  return pytesseract.image_to_data(Image.open('tests/download.png'),lang='hin+eng')
+
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+
+      # create a secure filename
+      filename = secure_filename(f.filename)
+
+      # save file to /static/uploads
+      filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
+      f.save(filepath)
+      
+      # load the example image and convert it to grayscale
+      image = cv2.imread(filepath)
+      gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+      
+      # apply thresholding to preprocess the image
+      gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+      # apply median blurring to remove any blurring
+      gray = cv2.medianBlur(gray, 3)
+
+      # save the processed image in the /static/uploads directory
+      ofilename = os.path.join(app.config['UPLOAD_FOLDER'],"{}.png".format(os.getpid()))
+      cv2.imwrite(ofilename, gray)
+      
+      # perform OCR on the processed image
+      text = pytesseract.image_to_string(Image.open(ofilename))
+      
+      # remove the processed image
+      os.remove(ofilename)
+
+      return jsonify({'text': text})
+
+if __name__ == '__main__':
+  port = int(os.environ.get('PORT', 5001))
+  app.run(debug=True, host='0.0.0.0', port=port)
+  # app.run(host="0.0.0.0", port=5000, debug=True)
