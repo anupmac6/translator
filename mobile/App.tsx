164cdc8568ddd3a410b8f743489701c2c047805a
anupmac6 <anupmac6@gmail.com>
2022-09-24 12:14:51 -0400

    adding google fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,10 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen'
+import {useFonts,Karla_200ExtraLight,
+  Karla_300Light,
+  Karla_400Regular,Karla_500Medium,Karla_600SemiBold,
+  Karla_700Bold,Karla_800ExtraBold} from '@expo-google-fonts/karla'
+import { useCallback } from 'react';
+import { Fonts } from './src/constants/fonts';
 
+  SplashScreen.preventAutoHideAsync()
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Karla_200ExtraLight,
+    Karla_300Light,
+    Karla_400Regular,
+    Karla_500Medium,
+    Karla_600SemiBold,
+    Karla_700Bold,
+    Karla_800ExtraBold
+  })
+
+  const onLayout = useCallback(async() => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync()
+    }
+  },[fontsLoaded])
+
+  if(!fontsLoaded) {
+    return null;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={styles.container} onLayout={onLayout}>
+      <Text style={{fontFamily:Fonts.Karla.Medium}}>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ mobile/src/constants/fonts.ts
@@ -0,0 +1,11 @@
+export const Fonts = {
+    Karla:{
+        ExtraLight: 'Karla_200ExtraLight',
+        Light: 'Karla_300Light',
+        Regular: 'Karla_400Regular',
+        Medium: 'Karla_500Medium',
+        SemiBold: 'Karla_600SemiBold',
+        Bold: 'Karla_700Bold',
+        ExtraBold: 'Karla_800ExtraBold'
+    }
+}
