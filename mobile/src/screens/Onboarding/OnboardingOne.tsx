0ce0a1924566b6b6e9dcf5700458839185a0e8e5
Anup Macwan <anup.macwan@willowtreeapps.com>
2022-09-24 23:51:38 -0400

    add onboarding

--- mobile/src/screens/Onboarding/OnboardingOne.tsx
+++ mobile/src/screens/Onboarding/OnboardingOne.tsx
@@ -1,12 +1,87 @@
-import { StyleSheet, Text, View,Dimensions } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { StyleSheet, Text, View, Dimensions, Pressable } from 'react-native';
+import React, { useCallback, useRef, useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Fonts } from '../../constants/fonts';
-import LottieView from 'lottie-react-native';
+import LottieView, { AnimationObject } from 'lottie-react-native';
+import lottie1 from '../../../assets/lottie/translate-1.json';
+import lottie2 from '../../../assets/lottie/translate-2.json';
+import lottie3 from '../../../assets/lottie/translate-3.json';
+import lottie4 from '../../../assets/lottie/translate-4.json';
+import lottie5 from '../../../assets/lottie/translate-5.json';
+import { useDispatch } from 'react-redux';
+import { setShowOnboarding } from '../../store/app/slice';
+import Animated, { Layout, SlideInRight } from 'react-native-reanimated';
 
-const {height} = Dimensions.get('screen')
+const { height, width } = Dimensions.get('screen');
+interface onboardingItem {
+  id: number;
+  lottie: object;
+  header: string;
+  subHeader: string;
+}
+
 const OnboardingOne = () => {
+  const items: onboardingItem[] = [
+    {
+      id: 0,
+      lottie: lottie1,
+      header: 'World Map with people',
+      subHeader:
+        'We are always ready to deliver your items quickly and professionally.',
+    },
+    {
+      id: 1,
+      lottie: lottie2,
+      header: 'Translate any languages',
+      subHeader:
+        'We are always ready to deliver your items quickly and professionally.',
+    },
+    {
+      id: 2,
+      lottie: lottie3,
+      header: 'Translate using  voice',
+      subHeader:
+        'We are always ready to deliver your items quickly and professionally.',
+    },
+    {
+      id: 3,
+      lottie: lottie4,
+      header: 'Translate using Camera',
+      subHeader:
+        'We are always ready to deliver your items quickly and professionally.',
+    },
+    {
+      id: 4,
+      lottie: lottie5,
+      header: 'Listen to your translation',
+      subHeader:
+        'We are always ready to deliver your items quickly and professionally.',
+    },
+  ];
+
+  const dispatch = useDispatch();
+
+  const [currentItem, setCurrentItem] = useState<number>(0);
+
+  const onSkipOrFinish = useCallback(() => {
+    dispatch(setShowOnboarding(false));
+  }, [dispatch]);
+
+  const onNextHandler = useCallback(() => {
+    if (currentItem !== items.length - 1) {
+      setCurrentItem((prevState) => prevState + 1);
+    } else {
+      onSkipOrFinish();
+    }
+  }, [currentItem, onSkipOrFinish]);
+
+  const onBackHandler = useCallback(() => {
+    if (currentItem !== 0) {
+      setCurrentItem((prevState) => prevState - 1);
+    }
+  }, [currentItem]);
+
   return (
     <SafeAreaView style={styles.screen}>
       <LinearGradient
@@ -14,64 +89,148 @@ const OnboardingOne = () => {
         colors={['rgba(175,225,175,0.6)', 'white']}
         style={styles.background}
       />
-      <View style={styles.skip}>
+      <Pressable style={styles.skip} onPress={onSkipOrFinish}>
         <Text style={styles.skipText}>Skip</Text>
-      </View>
-      <View style={styles.image}>
-      <LottieView
-        autoPlay
-        loop
+      </Pressable>
+      <View
         style={{
-          width: 200,
-          height: 200,
-          backgroundColor: 'translate',
+          flex: 2,
+          justifyContent: 'space-around',
         }}
-        // Find more Lottie files at https://lottiefiles.com/featured
-        source={require('../../../assets/lottie/translate-1.json')}
-      />
+      >
+        <View style={styles.image}>
+          <LottieView
+            autoPlay
+            loop
+            style={{
+              width: width - 40,
+              height: width - 40,
+              backgroundColor: 'transparent',
+            }}
+            // Find more Lottie files at https://lottiefiles.com/featured
+            source={items[currentItem].lottie as AnimationObject}
+          />
+        </View>
+        <View style={{}}>
+          <Animated.View
+            entering={SlideInRight}
+            layout={Layout.delay(100)}
+            style={styles.header}
+          >
+            <Text style={styles.headerText}>{items[currentItem].header}</Text>
+          </Animated.View>
+          <View style={styles.subtitle}>
+            <Text style={styles.subtitleText}>
+              {items[currentItem].subHeader}
+            </Text>
+          </View>
+        </View>
       </View>
-      <View style={styles.header}>
-        <Text style={styles.headerText}>
-            Delivery everywhere
-        </Text>
+      <View style={{}}>
+        <View style={styles.actions}>
+          <Pressable
+            onPress={onBackHandler}
+            style={styles.screenIndicatorWrapper}
+          >
+            {items.map((item, index) => (
+              <View
+                key={item.id}
+                style={
+                  currentItem === index
+                    ? styles.screenIndicatorActive
+                    : styles.screenIndicator
+                }
+              ></View>
+            ))}
+          </Pressable>
+          <Pressable onPress={onNextHandler} style={styles.button}>
+            <Text style={styles.buttonText}>
+              {currentItem === items.length - 1 ? 'Finish' : 'Next'}
+            </Text>
+          </Pressable>
+        </View>
       </View>
-      <View style={styles.subtitle}>
-        <Text style={styles.subtitleText}>
-            We are always ready to deliver your items quickly and professionally.
-        </Text>
-      </View>
-      <Text>OnboardingOne</Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default OnboardingOne
+export default OnboardingOne;
 
 const styles = StyleSheet.create({
-    screen:{
-        flex: 1,
-        backgroundColor: '#fff',
-        paddingHorizontal: 20,
-    },
-    background: {
-        position: 'absolute',
-        left: 0,
-        right: 0,
-        top: 0,
-        height: height/1.2,
-      },
-      skip:{
-        marginTop:10,
-        flexDirection: 'row',
-        justifyContent:'flex-end'
-      },
-      skipText:{
-        fontFamily: Fonts.Karla.Medium,
-        fontSize: 16
-      },
-      image:{},
-      header:{},
-      headerText:{},
-      subtitle:{},
-      subtitleText:{}
-})
+  screen: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 20,
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: height / 1.6,
+  },
+  skip: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+  },
+  skipText: {
+    fontFamily: Fonts.Karla.Medium,
+    fontSize: 16,
+  },
+  image: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  header: {
+    marginBottom: 20,
+  },
+  headerText: {
+    fontFamily: Fonts.Karla.Bold,
+    fontSize: 24,
+    lineHeight: 30,
+    width: '50%',
+  },
+  subtitle: {},
+  subtitleText: {
+    fontFamily: Fonts.Karla.Regular,
+    fontSize: 16,
+    lineHeight: 22,
+    width: '80%',
+  },
+  actions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  screenIndicatorWrapper: {
+    flexDirection: 'row',
+  },
+  screenIndicator: {
+    height: 10,
+    width: 10,
+    borderRadius: 10,
+    backgroundColor: 'lightgray',
+    marginRight: 4,
+  },
+  screenIndicatorActive: {
+    height: 10,
+    width: 20,
+    borderRadius: 10,
+    backgroundColor: 'green',
+    marginRight: 4,
+  },
+  button: {
+    backgroundColor: 'rgb(175,225,175)',
+    paddingHorizontal: 25,
+    paddingVertical: 6,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontFamily: Fonts.Karla.Medium,
+    fontSize: 16,
+    lineHeight: 22,
+    color: 'white',
+  },
+});
